f070acffd73e3164e58adb826e7b7088342e56e4
artskoczylas <art.skoczylas@gmail.com>
2024-03-10 20:40:51 +0100

    feat: Display username in layout

---
+++ src/PortfolioEye/PortfolioEye.Client/Extensions/IdentityExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioEye.Client.Extensions
+{
+    public static class IdentityExtension
+    {
+        public static string GetInitials(this System.Security.Principal.IIdentity identity)
+        {
+            return (identity.Name != null && identity.Name.Length > 2) ? identity.Name.Substring(0, 2).ToUpper() : "NA";
+        }
+    }
+}
