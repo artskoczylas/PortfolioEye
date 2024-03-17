2691d2c4df6abb62f469c916e5d9bee4d9038ecd
artskoczylas <art.skoczylas@gmail.com>
2024-03-17 16:36:57 +0100

    fix: add missing httpclientfactory extension class

---
+++ src/PortfolioEye/PortfolioEye.Client.Infrastructure/Extensions/HttpClientFactoryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioEye.Client.Infrastructure.Extensions
+{
+	public static class HttpClientFactoryExtensions
+	{
+		public static HttpClient MainApiClient(this IHttpClientFactory factory) => factory.CreateClient("MainApi");
+	}
+}
