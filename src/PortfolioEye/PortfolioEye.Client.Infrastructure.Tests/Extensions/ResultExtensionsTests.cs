ce1e29dd961622f07e5ece3450ff269f590c40e5
artskoczylas <art.skoczylas@gmail.com>
2024-03-19 07:43:50 +0100

    feat: Add more unit tests for HttpClientFactoryExtensions

--- src/PortfolioEye/PortfolioEye.Client.Infrastructure.Tests/Extensions/ResultExtensionsTests.cs
+++ src/PortfolioEye/PortfolioEye.Client.Infrastructure.Tests/Extensions/ResultExtensionsTests.cs
@@ -1,55 +1,102 @@
 ﻿using System.Net;
 using System.Text.Json;
-using eastsoft.RCP.Shared.Wrappers;
 using FluentAssertions;
 using PortfolioEye.Client.Infrastructure.Extensions;
-namespace PortfolioEye.Infrastructure.Tests.Extensions;
+using PortfolioEye.Common.Wrappers;
 
-
+namespace PortfolioEye.Client.Infrastructure.Tests.Extensions;
 
 public class HttpClientFactoryExtensionsTests
 {
     private record TestDataRecord(string Abc);
-    [SetUp]
-    public void Setup()
-    {
-        
-    }
+
     [Test]
     public async Task ToResult_WithSuccessfulResponse_ReturnsValidFlag()
     {
         var response = await PrepareSuccessResponse(new TestDataRecord("Valid data"));
-        
+
         var result = await response.ToResult<TestDataRecord>();
-        
+
         result.IsSuccess.Should().BeTrue();
     }
+
     [Test]
     public async Task ToResult_WithSuccessfulResponse_ReturnsEmptyMessages()
     {
         var response = await PrepareSuccessResponse(new TestDataRecord("Valid data"));
-        
+
         var result = await response.ToResult<TestDataRecord>();
-        
+
         result.Messages.Should().NotBeNull();
         result.Messages.Should().BeEmpty();
     }
+
     [Test]
-    public async Task ToResult_WithSuccessfulResponse_ReturnsValidResult()
+    public async Task ToResult_WithSuccessfulResponse_ReturnsValidData()
     {
         var response = await PrepareSuccessResponse(new TestDataRecord("Valid data"));
-        
+
         var result = await response.ToResult<TestDataRecord>();
-        
+
         result.Data.Should().NotBeNull();
         result.Data!.Abc.Should().Be("Valid data");
     }
-    
+
+    [Test]
+    public async Task ToResult_WithFailedResponse_ReturnsValidFlag()
+    {
+        var response = await PrepareFailResponse(HttpStatusCode.NotFound,"Some error");
+
+        var result = await response.ToResult<TestDataRecord>();
+
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    [Test]
+    public async Task ToResult_WithFailedResponse_ReturnsValidMessage()
+    {
+        var response = await PrepareFailResponse(HttpStatusCode.NotFound,"Some error");
+
+        var result = await response.ToResult<TestDataRecord>();
+
+        result.Messages.Should().HaveCount(1);
+        result.Messages.First().Should().Be("Some error");
+    }
+
+    [Test]
+    public async Task ToResult_WithFailedResponse_ReturnsValidErrorCode()
+    {
+        var response = await PrepareFailResponse(HttpStatusCode.NotFound,"Some error");
+
+        var result = await response.ToResult<TestDataRecord>();
+
+        result.ErrorCode.Should().Be(404);
+    }
+
+    [Test]
+    public async Task ToResult_WithFailedResponse_ReturnsEmptyData()
+    {
+        var response = await PrepareFailResponse(HttpStatusCode.NotFound,"Some error");
+
+        var result = await response.ToResult<TestDataRecord>();
+
+        result.Data.Should().BeNull();
+    }
+
     private async Task<HttpResponseMessage> PrepareSuccessResponse(TestDataRecord record)
     {
         return new HttpResponseMessage(HttpStatusCode.OK)
         {
             Content = new StringContent(JsonSerializer.Serialize(await Result<TestDataRecord>.SuccessAsync(record)))
+        };
+    }
+
+    private async Task<HttpResponseMessage> PrepareFailResponse(HttpStatusCode code, string message)
+    {
+        return new HttpResponseMessage(code)
+        {
+            Content = new StringContent(
+                JsonSerializer.Serialize(await Result<TestDataRecord>.FailAsync((int)code, message)))
         };
     }
 }
