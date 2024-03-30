dad889163bc7229ca4076b4e934337d8f545c996
artskoczylas <art.skoczylas@gmail.com>
2024-03-30 22:47:11 +0100

    feat: Crop profile image

--- src/PortfolioEye/PortfolioEye.Infrastructure/Handlers/Users/Commands/UploadProfilePhotoByUserIdCommandHandler.cs
+++ src/PortfolioEye/PortfolioEye.Infrastructure/Handlers/Users/Commands/UploadProfilePhotoByUserIdCommandHandler.cs
@@ -12,9 +12,9 @@ public class UploadProfilePhotoByUserIdCommandHandler(ApplicationDbContext dbCon
 {
     public async Task<IResult> Handle(UploadProfilePhotoByUserIdCommand request, CancellationToken cancellationToken)
     {
-        SKBitmap bitmap = SKBitmap.Decode(request.Content);
         SKImage image = SKImage.FromEncodedData(Convert.FromBase64String(request.Content));
-        SKData png = image.Encode(SKEncodedImageFormat.Png, 95);
+
+        var png = CropImage(image);
 
         var photosDirectory = new DirectoryInfo("Data/ProfilePhotos");
         if (!photosDirectory.Exists)
@@ -34,5 +34,35 @@ public class UploadProfilePhotoByUserIdCommandHandler(ApplicationDbContext dbCon
         dbContext.Update(user);
         await dbContext.SaveChangesAsync(cancellationToken);
         return await Result.SuccessAsync();
+    }
+
+    private SKData CropImage(SKImage image)
+    {
+        SKBitmap bitmap = SKBitmap.FromImage(image);
+
+        using var pixmap =  new SKPixmap(bitmap.Info, bitmap.GetPixels());
+        int left = 0;
+        int top = 0;
+        int right = 0;
+        int bottom = 0;
+        var horizontal = bitmap.Width > bitmap.Height;
+        if (horizontal)
+        {
+            top = 0;
+            bottom = bitmap.Height;
+            left = (bitmap.Height - bitmap.Width) / 2;
+            right = left + bitmap.Width;
+        }
+        else
+        {
+            top = (bitmap.Width - bitmap.Height) / 2;
+            bottom = top + bitmap.Height;
+            left = 0;
+            right = bitmap.Width;
+        }
+        SkiaSharp.SKRectI rectI = new SkiaSharp.SKRectI(0, 0, 105, 105);
+        var subset = pixmap.ExtractSubset(rectI);
+        SKData png = subset.Encode(SKEncodedImageFormat.Png, 95);
+        return png;
     }
 }
