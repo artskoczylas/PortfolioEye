0ba046bd22a808725c80e6088346633600dd8aec
artskoczylas <art.skoczylas@gmail.com>
2024-04-02 08:09:03 +0200

    feat: splash screen - little hacky but it works

---
+++ src/PortfolioEye/PortfolioEye/wwwroot/app.js
@@ -0,0 +1,8 @@
+﻿(function () {
+
+    window.deleteElementById = function (id) {
+        var element = document.getElementById(id);
+        element.parentNode.removeChild(element);
+    }
+
+})();
