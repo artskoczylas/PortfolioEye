c75936c70bc2f3a50bf81b6be1616090dc50e6d3
artskoczylas <art.skoczylas@gmail.com>
2024-05-15 16:33:22 +0200

    feat: Add Last stock prices view

---
+++ src/PortfolioEye/PortfolioEye.Infrastructure/Migrations/20240515142340_AddCurrentStockPricesView.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioEye.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrentStockPricesView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 CREATE VIEW LastStockPrices AS
+                                     WITH LastDates AS (
+                                        SELECT "Ticker", max("Date") AS "Date"
+                                        FROM public."StockPrices" SP GROUP BY "Ticker"
+                                        )
+                                     
+                                     SELECT SP.* FROM LastDates LD
+                                     INNER JOIN public."StockPrices" SP ON SP."Ticker" = LD."Ticker" AND SP."Date" = LD."Date";
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW LastStockPrices;");
+        }
+    }
+}
