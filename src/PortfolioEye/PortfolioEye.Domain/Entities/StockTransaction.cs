be72e4940411f5f873edb1bd2b1825bb7f576135
artskoczylas <art.skoczylas@gmail.com>
2024-05-18 23:37:24 +0200

    feat: StockCurrencyColumnsInTransaction

--- src/PortfolioEye/PortfolioEye.Domain/Entities/StockTransaction.cs
+++ src/PortfolioEye/PortfolioEye.Domain/Entities/StockTransaction.cs
@@ -10,16 +10,17 @@ public class StockTransaction
     public Transaction Transaction { get; set; } = null!;
     public StockTransactionSide Side { get; set; }
     [StringLength(9)] public string Ticker { get; set; } = null!;
-    [Precision(18,8)]
-    public decimal Quantity { get; set; }
-    [Precision(18,6)]
-    public decimal Price { get; set; }
+    [Precision(18, 8)] public decimal Quantity { get; set; }
+    [Precision(18, 6)] public decimal Price { get; set; }
 
     [StringLength(3)] public string Currency { get; set; } = null!;
-    [Precision(18,2)]
-    public decimal FeeValue { get; set; }
+    [Precision(18, 2)] public decimal FeeValue { get; set; }
 
     [StringLength(3)] public string FeeCurrency { get; set; } = null!;
+
+    [Precision(18, 6)] public decimal PriceInStockCurrency { get; set; }
+    [Precision(18, 2)] public decimal ValueInStockCurrency { get; set; }
+    [StringLength(3)] public string StockCurrency { get; set; } = null!;
 }
 
 public enum StockTransactionSide
---
+++ src/PortfolioEye/PortfolioEye.Infrastructure/Migrations/20240518213637_AddStockTransactionStockValueColumns.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioEye.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStockTransactionStockValueColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PriceInStockCurrency",
+                table: "StockTransactions",
+                type: "numeric(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StockCurrency",
+                table: "StockTransactions",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValueInStockCurrency",
+                table: "StockTransactions",
+                type: "numeric(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceInStockCurrency",
+                table: "StockTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "StockCurrency",
+                table: "StockTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "ValueInStockCurrency",
+                table: "StockTransactions");
+        }
+    }
+}
