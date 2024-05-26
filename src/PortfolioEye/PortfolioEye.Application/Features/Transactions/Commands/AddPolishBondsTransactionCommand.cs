0241ee58832e7ea600adce4bd9dadd09941f3bea
artskoczylas <art.skoczylas@gmail.com>
2024-05-26 21:54:12 +0200

    feat: Form for add bond transaction

---
+++ src/PortfolioEye/PortfolioEye.Application/Features/Transactions/Commands/AddPolishBondsTransactionCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using PortfolioEye.Common.Wrappers;
+
+namespace PortfolioEye.Application.Features.Transactions.Commands;
+
+public record AddPolishBondsTransactionForUserCommand(Guid UserId, AddPolishBondsTransactionCommand Inner)
+    : AddPolishBondsTransactionCommand(Inner);
+public record AddPolishBondsTransactionCommand(
+    Guid PortfolioId,
+    Guid AccountId,
+    BondTransactionSide Side,
+    DateOnly TransactionDate,
+    BondKind Kind,
+    int Quantity)
+    : IRequest<IResult>;
+    
+public enum BondTransactionSide
+{
+    Purchase,
+    Sale
+}
+
+public enum BondKind
+{
+    EDO,
+    ROD
+}
